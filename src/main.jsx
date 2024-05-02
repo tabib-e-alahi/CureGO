71c3d285c0b38a5705b300256c31e33ac666fdbf
tabib-e-alahi <etabib.alahi@gmail.com>
2024-05-02 21:34:25 +0600

    auth provider initial setup done

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./Routes/routes.jsx";
+import AuthProvider from "./providers/AuthProvider/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
---
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import app from "../../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
+
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+    const authInfo = {}
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
