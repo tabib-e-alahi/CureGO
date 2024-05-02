39ffac13ebf543db76752f7628a83cc32abf1e2d
tabib-e-alahi <etabib.alahi@gmail.com>
2024-05-02 22:02:54 +0600

    authProvider.jsx folder setUp done

--- src/providers/AuthProvider/AuthProvider.jsx
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -1,18 +1,60 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import app from "../../firebase/firebase.config";
-import { getAuth } from "firebase/auth";
-
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 export const AuthContext = createContext(null);
-const auth = getAuth(app)
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = ({children}) => {
-    const authInfo = {}
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // ------------ for creating a new user =========
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() =>{
+    const unSubscribe = onAuthStateChanged(auth, currenUser =>{
+        console.log('Inside on state change:',currenUser);
+        setUser(currenUser);
+        setLoading(false);
+    });
+    return () =>{
+        unSubscribe()
+    }
+},[])
+
+
+  const authInfo = { user, loading, createUser, signIn, googleSignIn, logOut };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
