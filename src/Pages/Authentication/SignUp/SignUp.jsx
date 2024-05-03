c425b00c09c1d650f0fd255f9110e60ea4c5e5af
tabib-e-alahi <etabib.alahi@gmail.com>
2024-05-04 00:40:26 +0600

    sign up style done only text editing remain

--- src/Pages/Authentication/SignUp/SignUp.jsx
+++ src/Pages/Authentication/SignUp/SignUp.jsx
@@ -1,131 +1,105 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import SocialLogin from "../../../SharedComponents/SocialLogin/SocialLogin";
 import { useForm } from "react-hook-form";
-import './SignUp.css'
-
+import "./SignUp.css";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    // reset,
+    formState: { errors },
+  } = useForm();
 
-    const {
-        register,
-        handleSubmit,
-        // reset,
-        formState: { errors },
-      } = useForm();
-    
-      const onSubmit = (data) => {
-        console.log(data)
-      }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    return (
-        <div className="mx-auto p-4 lg:col-span-3">
-      <h1 className="text-center text-3xl font-bold mt-8 mb-2 text-[#172734]">
-        Register your account
-      </h1>
-      <div className="register-form mb-6 p-10 w-80 lg:w-96 mx-auto rounded-sm">
-        <form className="mb-6 px-4" onSubmit={handleSubmit(onSubmit)}>
-          {/* Personal Information================== */}
-          <div className="flex flex-col gap-6">
-            
-
-            
-
-            
-
-            
+  return (
+    <div className="mx-auto w-2/3 my-10  bg-white p-10">
+      <SocialLogin></SocialLogin>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid grid-cols-2 gap-10 "
+      >
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="" className="pb-1 text-black text-sm custome_font_family">
+              Your name
+            </label>
+            <input
+              {...register("name", { required: true })}
+              type="text"
+              className="p-3  form-input"
+              placeholder="Tabib E Alahi"
+            />
+            {errors.name && (
+              <span className="text-red-500">This field is required</span>
+            )}
           </div>
-        </form>
-        <SocialLogin></SocialLogin>
-        <div className="flex justify-center items-center gap-1">
-          <p className="font-normal">Already have an account?</p>
-          <Link to="/login">
-            <button className="btn btn-link pl-0">Login</button>
-          </Link>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="" className="pb-1 text-black text-sm custome_font_family">
+              Photo URL
+            </label>
+            <input
+              {...register("photoURL", { required: true })}
+              type="text"
+              className="p-3  form-input"
+              placeholder="url...."
+            />
+            {errors.photoURL && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="" className="pb-1 text-black text-sm custome_font_family">
+              Enter Email
+            </label>
+            <input
+              {...register("email", { required: true })}
+              type="email"
+              className="p-3  form-input"
+              placeholder="example@example.com"
+            />
+            {errors.email && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
         </div>
-      </div>
-
-
-<form className="grid grid-cols-2 gap-10 w-2/3 mx-auto shadow-md shadow-black p-10 my-10">
-  <div>
-  <div className="flex flex-col gap-1">
-              <label htmlFor="" className="pb-1">
-                Your name
-              </label>
-              <input
-                {...register("name", { required: true })}
-                type="text"
-                className="p-3 rounded-lg form-input "
-                placeholder="Tabib E Alahi"
-              />
-              {errors.name && (
-                <span className="text-red-500">This field is required</span>
-              )}
-            </div>
-            <div className="flex flex-col gap-1">
-              <label htmlFor="" className="pb-1">
-                Photo URL
-              </label>
-              <input
-                {...register("photoURL", { required: true })}
-                type="text"
-                className="p-3 rounded-lg form-input"
-                placeholder="url...."
-              />
-              {errors.photoURL && (
-                <span className="text-red-500">This field is required</span>
-              )}
-            </div>
-            <div className="flex flex-col gap-1">
-              <label htmlFor="" className="pb-1">
-                Enter Email
-              </label>
-              <input
-                {...register("email", { required: true })}
-                type="email"
-                className="p-3 rounded-lg form-input"
-                placeholder="example@example.com"
-              />
-              {errors.email && (
-                <span className="text-red-500">This field is required</span>
-              )}
-            </div>
-  </div>
-  <div>
-  <div className="flex flex-col gap-1">
-              <label htmlFor="" className="pb-1">
-                Enter Password
-              </label>
-              <input
-                {...register("password", {
-                  required: true,
-                  minLength: 6
-                })}
-                type="password"
-                className="p-3 rounded-lg form-input"
-                placeholder="password@123"
-              />
-              {errors.password?.type === "required" && (
-                <p role="alert" className="text-red-500">
-                  Password is required
-                </p>
-              )}
-              {errors.password?.type === "minLength" && (
-                <p role="alert" className="text-red-500">
-                  Password must be 6 characters minimum
-                </p>
-              )}
-            </div>
-            <div className="w-2/3 mx-auto text-center">
-              <button className="px-6 py-2  rounded-md text-white w-full bg-[#52ab98]">
-                Register
-              </button>
-            </div>
-  </div>
-</form>
-
-      
+        <div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="" className="pb-1 text-black text-sm custome_font_family">
+              Enter Password
+            </label>
+            <input
+              {...register("password", {
+                required: true,
+                minLength: 6,
+              })}
+              type="password"
+              className="p-3 rounded-lg form-input"
+              placeholder="password@123"
+            />
+            {errors.password?.type === "required" && (
+              <p role="alert" className="text-red-500">
+                Password is required
+              </p>
+            )}
+            {errors.password?.type === "minLength" && (
+              <p role="alert" className="text-red-500">
+                Password must be 6 characters minimum
+              </p>
+            )}
+          </div>
+          <div className="w-2/3 mt-4 mx-auto text-center">
+            <button className="px-6 py-2  rounded-md text-white w-full bg-[#52ab98]">
+              Register
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-    );
+  );
 };
 
 export default SignUp;
--- src/SharedComponents/SocialLogin/SocialLogin.jsx
+++ src/SharedComponents/SocialLogin/SocialLogin.jsx
@@ -2,42 +2,52 @@ import { FcGoogle } from "react-icons/fc";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 
-
 const SocialLogin = () => {
+  const { googleSignIn } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  // const axiosPublic = useAxiosPublic()
 
-    const {googleSignIn} = useAuth()
-    const navigate = useNavigate();
-    const location = useLocation();
-    // const axiosPublic = useAxiosPublic()
+  const from = location.state?.from?.pathname || "/";
 
-    const from = location.state?.from?.pathname || "/";
-    
-    const handleGoogleSignIn = () => {
-        googleSignIn().then((result) => {
-          console.log(result.user);
-          const userInfo = {
-            user_Name: result.user?.displayName,
-            user_Email: result.user?.email,
-            user_Profile_Picture: result.user?.photoURL
-          };
-          console.log(userInfo);
-        //   axiosPublic.post("/users", userInfo).then((res) => {
-        //     console.log(res.data);
-        //     navigate("/");
-        //   });
-        navigate(from, { replace: true });
-        });
+  const handleGoogleSignIn = () => {
+    googleSignIn().then((result) => {
+      console.log(result.user);
+      const userInfo = {
+        user_Name: result.user?.displayName,
+        user_Email: result.user?.email,
+        user_Profile_Picture: result.user?.photoURL,
       };
+      console.log(userInfo);
+      //   axiosPublic.post("/users", userInfo).then((res) => {
+      //     console.log(res.data);
+      //     navigate("/");
+      //   });
+      navigate(from, { replace: true });
+    });
+  };
 
-    return (
-        <div className="flex flex-col justify-center gap-6 mb-4">
-        <div className="divider">OR</div>
-        <button onClick={handleGoogleSignIn} className="flex items-center justify-center w-fit mx-auto bg-white  border rounded-lg  px-6 py-2 text-sm font-medium text-gray-800  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+  return (
+    <div className="flex flex-col justify-center gap-6">
+      <div className="flex justify-center items-center gap-6">
+        <button
+          onClick={handleGoogleSignIn}
+          className="flex items-center justify-center  bg-white  border rounded-lg  px-6 py-2 text-sm font-medium text-gray-800  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          <FcGoogle className="h-6 w-6 mr-2"></FcGoogle>
+          <span>Continue with Google</span>
+        </button>
+        <button
+          onClick={handleGoogleSignIn}
+          className="flex items-center justify-center  bg-white  border rounded-lg  px-6 py-2 text-sm font-medium text-gray-800  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
           <FcGoogle className="h-6 w-6 mr-2"></FcGoogle>
           <span>Continue with Google</span>
         </button>
       </div>
-    );
+      <div className="divider">OR</div>
+    </div>
+  );
 };
 
 export default SocialLogin;
