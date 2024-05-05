abf095d98268a8b5535619108016f8b5fffa9ce7
tabib-e-alahi <etabib.alahi@gmail.com>
2024-05-05 10:59:01 +0600

    social login section added

--- src/SharedComponents/SocialLogin/SocialLogin.jsx
+++ src/SharedComponents/SocialLogin/SocialLogin.jsx
@@ -1,6 +1,7 @@
 import { FcGoogle } from "react-icons/fc";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
+import facebook from '../../assets/facebook.png'
 
 const SocialLogin = () => {
   const { googleSignIn } = useAuth();
@@ -28,24 +29,26 @@ const SocialLogin = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center gap-6">
+    <div className="flex flex-col justify-center gap-2 mb-4">
       <div className="flex justify-center items-center gap-6">
         <button
           onClick={handleGoogleSignIn}
-          className="flex items-center justify-center  bg-white  border rounded-lg  px-6 py-2 text-sm font-medium text-gray-800  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          className="flex items-center justify-center  bg-white  border rounded-sm px-12 py-3  font-medium text-gray-800  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
-          <FcGoogle className="h-6 w-6 mr-2"></FcGoogle>
+          <FcGoogle className="h-8 w-8 mr-2"></FcGoogle>
           <span>Continue with Google</span>
         </button>
         <button
           onClick={handleGoogleSignIn}
-          className="flex items-center justify-center  bg-white  border rounded-lg  px-6 py-2 text-sm font-medium text-gray-800  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          className="flex items-center justify-center  bg-white  border rounded-sm px-12 py-3  font-medium text-gray-800  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
-          <FcGoogle className="h-6 w-6 mr-2"></FcGoogle>
-          <span>Continue with Google</span>
+          <img className="h-8 w-8 mr-2" src={facebook} alt="FB" />
+          {/* <FcGoogle className="h-8 w-8 mr-2"></FcGoogle> */}
+          <span>Continue with Facebbok</span>
         </button>
+       
       </div>
-      <div className="divider">OR</div>
+      <div className="divider text-gray-500 text-sm font-medium">Or continue with email account</div>
     </div>
   );
 };
