3f9756831a4617b1d09fde295483c35df4cfa024
tabib-e-alahi <etabib.alahi@gmail.com>
2024-05-08 23:33:21 +0600

    all product properties listed down in object

--- src/Pages/ProductDestails/ProductDetails.jsx
+++ src/Pages/ProductDestails/ProductDetails.jsx
@@ -13,10 +13,29 @@ const ProductDetails = () => {
     },
   });
   console.log(product);
+
+  const {
+    product_name,
+    product_image,
+    brand_name,
+    reviews,
+    product_price,
+    product_stock_count,
+    product_subtitle,
+    product_overview,
+    key_ingredients,
+    key_ingredients_description,
+    product_usage,
+    skin_concerns,
+    ingredients,
+    product_quantity,
+    return_policy,
+  } = product;
+
   return (
     <div className="bg-white  w-96">
-      <img className="w-60 h-80" src={product?.product_image} alt="" />
-      <h1 className="text-4xl text-black mt-20">{product?.product_name}</h1>
+      <img className="w-60 h-80" src={product_image} alt="" />
+      <h1 className="text-4xl text-black mt-20">{product_name}</h1>
     </div>
   );
 };
